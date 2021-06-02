d78833e28d637975fbc785d37134ba47bbfd3bcc
xilni <xilni@users.noreply.github.com>
2021-06-01 23:45:21 -0400

    feat: initial release of SeatruckSolar

---
+++ SeatruckSolar/Modules/SeatruckSolarChargerModule.cs
@@ -0,0 +1,53 @@
+﻿using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SeatruckSolar.Modules
+{
+    class SeatruckSolarChargerModule : Equipable
+    {
+        public SeatruckSolarChargerModule() : base(
+            "SeatruckSolarCharger",
+            "Seatruck Solar Charger",
+            "Recharges the Seatruck's power cells while in sunlight.")
+        { }
+
+        public override EquipmentType EquipmentType => EquipmentType.SeaTruckModule;
+        public override TechType RequiredForUnlock => TechType.SeaTruck;
+        public override TechGroup GroupForPDA => TechGroup.VehicleUpgrades;
+        public override TechCategory CategoryForPDA => TechCategory.VehicleUpgrades;
+        public override CraftTree.Type FabricatorType => CraftTree.Type.SeamothUpgrades;
+        public override string[] StepsToFabricatorTab => new string[] { "SeaTruckUpgrade" };
+        public override QuickSlotType QuickSlotType => QuickSlotType.Passive;
+
+        public override IEnumerator GetGameObjectAsync(IOut<GameObject> gameObject)
+        {
+            CoroutineTask<GameObject> task = CraftData.GetPrefabForTechTypeAsync(TechType.SeamothSolarCharge);
+            yield return task;
+            GameObject originalPrefab = task.GetResult();
+            GameObject resultPrefab = GameObject.Instantiate(originalPrefab);
+            gameObject.Set(resultPrefab);
+        }
+
+        // Same recipe as old Seamoth Solar Charger
+        protected override RecipeData GetBlueprintRecipe()
+        {
+            return new RecipeData()
+            {
+                craftAmount = 1,
+                Ingredients = new List<Ingredient>()
+                {
+                    new Ingredient(TechType.AdvancedWiringKit, 1),
+                    new Ingredient(TechType.EnameledGlass, 1)
+                }
+            };
+        }
+
+        protected override Sprite GetItemSprite()
+        {
+            return SpriteManager.Get(TechType.SeamothSolarCharge);
+        }
+    }
+}
---
+++ SeatruckSolar/SeatruckSolar.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+
+namespace SeatruckSolar
+{
+    [QModCore]
+    public class SeatruckSolar
+    {
+        internal static Modules.SeatruckSolarChargerModule seatruckSolarModule = new Modules.SeatruckSolarChargerModule();
+
+        [QModPatch]
+        public static void Load()
+        {
+            seatruckSolarModule.Patch();
+
+            Harmony harmony = new Harmony("com.xilni.seatrucksolar");
+            harmony.PatchAll();
+        }
+    }
+}
