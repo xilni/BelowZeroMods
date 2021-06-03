41fa8e9a5ab891b826397d89c381635e55ccb4ae
xilni <xilni@users.noreply.github.com>
2021-06-03 19:00:10 -0400

    fix: NullReferenceException when piloting seatruck modules with thermal module

---
+++ SeatruckThermal/Behaviours/SeatruckThermalBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SeatruckThermal.Behaviours
+{
+    class SeatruckThermalBehaviour : MonoBehaviour
+    {
+        public SeaTruckUpgrades seatruck;
+
+        // Values derived from a linear regression on the Exosuit Thermal Reactor and implemented here
+        // so I don't have to copy over all the associated code that only lives there.
+        static float getThermalVal(float x)
+        {
+            return Mathf.Clamp((float)(x * 0.0342296 - 1.154310), 0, 1);
+        }
+
+        public void Update()
+        {
+            var waterSim = WaterTemperatureSimulation.main;
+            var temperature = waterSim.GetTemperature(seatruck.transform.position);
+            var num = getThermalVal(temperature);
+            seatruck.relay.AddEnergy(num * Time.deltaTime, out float amountStored);
+        }
+    }
+}
---
+++ SeatruckThermal/Patches/SeaTruckUpgrades_OnUpgradeModuleChange_Patch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using SeatruckThermal.Behaviours;
+using UnityEngine;
+
+namespace SeatruckThermal.Patches
+{
+    [HarmonyPatch]
+    internal class SeaTruckUpgrades_OnUpgradeModuleChange_Patch
+    {
+        [HarmonyPatch(typeof(SeaTruckUpgrades), nameof(SeaTruckUpgrades.OnUpgradeModuleChange))]
+        [HarmonyPostfix]
+        public static void Postfix(SeaTruckUpgrades __instance, int slotID, TechType techType, bool added)
+        {
+            SeatruckThermalBehaviour thermalModule = null;
+            var noneEquipped = false;
+            var foundBehaviour = false;
+
+            // We only care about the Seatruck Thermal Reactor, return otherwise
+            if (techType != SeatruckThermal.seatruckThermalModule.TechType)
+            {
+                return;
+            }
+
+            noneEquipped = __instance.modules.GetCount(techType) < 1;
+            foundBehaviour = __instance.gameObject.TryGetComponent(out thermalModule);
+
+            // If equipped, proceed
+            if (added && !foundBehaviour)
+            {
+                thermalModule = __instance.gameObject.AddComponent<SeatruckThermalBehaviour>();
+                thermalModule.seatruck = __instance;
+            }
+
+            if (!added && noneEquipped && foundBehaviour)
+                Object.Destroy(thermalModule);
+        }
+    }
+}
